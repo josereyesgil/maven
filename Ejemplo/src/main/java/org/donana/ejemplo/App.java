11508759b7b101c1ac0992a1ad82774f3c85f3d7
Jose Reyes Gil Rubio <josereyesgil@gmail.com>
2020-02-10 09:38:08 +0100

    Proyecto Maven

---
+++ Ejemplo/src/main/java/org/donana/ejemplo/App.java
@@ -0,0 +1,13 @@
+package org.donana.ejemplo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
